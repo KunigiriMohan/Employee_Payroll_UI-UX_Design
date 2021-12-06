1d4100327d576cd4e3edb859d2d8d62ce4ea1640
KunigiMohan <kmohanr5@gmail.com>
2021-12-06 11:57:52 +0530

    Added : EmployeePayroll Class for creating Objects of employee.

---
+++ js/EmployeePayroll.js
@@ -0,0 +1,74 @@
+class EmployeePayrollData{
+    /*Getters and Setters*/
+    get id(){
+        return this._id;
+    } 
+    set id(id){
+        this._id=id;
+    }
+
+    get name(){
+        return this._name;
+    }
+    set name(name){
+        let nameRegex=RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+        if(nameRegex.test(name))
+        this._name=name;
+        else throw 'Name is Incorect!';
+    }
+
+    get profilePic(){
+        return this._profilePic;
+    }
+
+    set profilePic(profilePic){
+        this._profilePic=profilePic;
+    }
+
+    get gender(){
+        this._gender;
+    }
+
+    set gender(gender){
+        this._gender=gender;
+    }
+
+    get department(){
+        return this._department;
+    }
+
+    set department(department){
+        this._department=department;
+    }
+
+    get salary(){
+        return this._salary;
+    }
+
+    set salary(salary){
+        this._salary=salary;
+    }
+
+    get note(){
+        return this._note;
+    }
+
+    set note(note){
+        this._note=note;
+    }
+
+    get startDate(){
+        return this._startDate=this.startDate;
+    }
+
+    set startDate(startDate){
+        this.startDate=startDate;
+    }
+
+    toString(){
+        const options={year : 'numeric',month: 'long',day:'numeric'};
+        const empDate=!this.startDate? "undefined":this.startDate.toLocalDateString("en-US",options);
+
+        return "ID: "+this.id+" Name: "+this.name+" Gender: "+this.gender+" ProfilePic: "+this.profilePic+" Department: "+this.department+" Salary: "+this.salary+" StartDate: "+empDate+" Note: "+this.note;
+    }
+}
